c37faeb13fa7e1f7fb6ce9ec45ddaafe39a6f56a
Jack Witbrock Finlay <jack@jackfinlay.com>
2016-10-02 16:04:28 +1300

    Change class attributes to list. Add Test app.

---
+++ Jsonize.Test/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using JackWFinlay.Jsonize;
+using System.Threading.Tasks;
+
+namespace Jsonize_Test
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Test();
+            Console.ReadLine();
+        }
+
+        private static async void Test()
+        {
+            string result = await TestJsonizeAsString();
+            Console.WriteLine(result);
+            System.IO.File.WriteAllText(@"C:\Users\Public\WriteText.txt", result);
+        }
+
+        private static async Task<string> TestJsonizeAsString()
+        {
+
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                string url = @"http://jackfinlay.com";
+
+                HttpResponseMessage response = await client.GetAsync(url);
+
+                string html = await response.Content.ReadAsStringAsync();
+                //html = System.IO.File.ReadAllText(@"C:\Users\Public\file.html");
+                Jsonize jsonize = new Jsonize(html);
+
+                return jsonize.ParseHtmlAsJsonString();
+            }
+        }
+    }
+}
